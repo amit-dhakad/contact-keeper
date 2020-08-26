9bdedde7775387891abf502d1511389127f8c159
Amit Dhakad <amit0502dhakad@gmail.com>
2020-08-26 20:10:42 +0530

    added user validation and save user in db

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express()
 const connectDB = require('./config/db')
-
 //  Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/',(req,res) => res.json({msg:'Welcome to the contactkeeper api'}));
 
 //  Define routes
