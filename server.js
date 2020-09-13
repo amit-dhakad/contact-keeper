a82d06ab82f5f37c23a521fe48554aa82cdc9fd0
amit-dhakad <amit0502dhakad@gmail.com>
2020-09-13 17:07:14 +0530

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const connectDB = require('./config/db')
+const path = require('path')
 //  Connect Database
 connectDB();
 
@@ -14,5 +15,15 @@ app.get('/',(req,res) => res.json({msg:'Welcome to the contactkeeper api'}));
 app.use('/api/users',require('./routes/users'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/contacts',require('./routes/contacts'))
+
+// Serve static assets in production
+
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__direname, 'client', 'build','index.html')))
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
